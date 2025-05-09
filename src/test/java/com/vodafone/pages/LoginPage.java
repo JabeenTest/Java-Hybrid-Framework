f7171030ac1eeb236a0131655af345093b5b9baa
JabeenTest <mjmogal30@gmail.com>
2025-05-09 00:35:09 -0700

    Added Webdriverkeyword

--- src/test/java/com/vodafone/pages/LoginPage.java
+++ src/test/java/com/vodafone/pages/LoginPage.java
@@ -3,7 +3,9 @@ package com.vodafone.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage {
+import com.vodafone.base.WebDriverKeywords;
+
+public class LoginPage extends WebDriverKeywords {
  private By usernameLocator = By.name("username");
  private By passwordLocator = By.name("password");
  private By loginLocator = By.xpath("//button[normalize-space()='Login']");
@@ -12,15 +14,16 @@ public class LoginPage {
  private WebDriver driver;
 
  public LoginPage(WebDriver driver) {
+	 super(driver);
   this.driver = driver;
  }
 
  public void enterUsername(String username) {
-  driver.findElement(usernameLocator).sendKeys(username);
+	 super.typeOnElement(errorLocator, username);
  }
 
  public void enterPassword(String password) {
-  driver.findElement(passwordLocator).sendKeys(password);
+  super.typeOnElement(errorLocator, password);
  }
 
  public void clickOnLogin() {
